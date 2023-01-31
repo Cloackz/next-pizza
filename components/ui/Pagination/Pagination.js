99923bd2002ceab3ee8d47dcbe88ded1ffcb4fca
cloackz <cloackz@yandex.ru>
2023-01-31 17:40:54 +0400

    Add new branch

--- components/ui/Pagination/Pagination.js
+++ components/ui/Pagination/Pagination.js
@@ -7,35 +7,34 @@ import GridBlock from '/components/ui/GridBlock/GridBlock';
 import styles from './Pagination.module.scss';
 
 const Pagination = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const { totalCount, currentPage, perPage } = useSelector(selectItems);
+	const { totalCount, currentPage, perPage } = useSelector(selectItems);
 
-  const pageNumbers = [];
+	const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalCount / perPage); i++) {
-    pageNumbers.push(i);
-  }
+	for (let i = 1; i <= Math.ceil(totalCount / perPage); i++) {
+		pageNumbers.push(i);
+	}
 
-  return (
-    <GridBlock className={styles.Pagination}>
-      <ul className={styles.List}>
-        {pageNumbers.map((page, index) => (
-          <li className={classNames(styles.Item)} key={index}>
-            <button
-              className={classNames(
-                styles.Button,
-                currentPage === page ? styles.ButtonActive : null
-              )}
-              onClick={() => dispatch(setCurrentPage(page))}
-            >
-              {page}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </GridBlock>
-  );
+	console.log(123);
+
+	return (
+		<GridBlock className={styles.Pagination}>
+			<ul className={styles.List}>
+				{pageNumbers.map((page, index) => (
+					<li className={classNames(styles.Item)} key={index}>
+						<button
+							className={classNames(styles.Button, currentPage === page ? styles.ButtonActive : null)}
+							onClick={() => dispatch(setCurrentPage(page))}
+						>
+							{page}
+						</button>
+					</li>
+				))}
+			</ul>
+		</GridBlock>
+	);
 };
 
 export default Pagination;
